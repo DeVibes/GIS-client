cb5c0771894daff825f6ada651f5510037751a27
Mark <replay.mkp@gmail.com>
2020-10-07 06:37:14 +0300

    added categories

--- src/Data/MeetupCategories.js
+++ src/Data/MeetupCategories.js
@@ -1,16 +1,34 @@
 import React from 'react'
 import DirectionsCarIcon from '@material-ui/icons/DirectionsCar';
 import SportsEsportsIcon from '@material-ui/icons/SportsEsports';
+import LocalBarIcon from '@material-ui/icons/LocalBar';
+import DirectionsRunIcon from '@material-ui/icons/DirectionsRun';
+import MenuBookIcon from '@material-ui/icons/MenuBook';
 
 export const MeetupCategories = [
+    {
+        value: `beer`,
+        label: `Beer night`,
+        icon: <LocalBarIcon/>
+    },
+    {
+        value: `games`,
+        label: `Board games`,
+        icon: <SportsEsportsIcon/>
+    },
     {
         value: `cars`,
         label: `Cars`,
         icon: <DirectionsCarIcon/>
     },
     {
-        value: `games`,
-        label: `Board games`,
-        icon: <SportsEsportsIcon/>
+        value: `sport`,
+        label: `Sport event`,
+        icon: <DirectionsRunIcon/>
+    },
+    {
+        value: `lecture`,
+        label: `Tech lecture`,
+        icon: <MenuBookIcon/>
     },
 ]
